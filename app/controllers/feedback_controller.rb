4ffc8e31da8326d8cc23ea9d442d980d06cf2330
Iain Kinnish <iainkinnish@googlemail.com>
2018-04-22 23:53:12 +0100

    Further changes to make API work

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -3,11 +3,20 @@ require 'net/http'
 class FeedbackController < ApplicationController
 
   def request_feedback
-    render html: '<div>Testing</div>'.html_safe
+    message = params[:text].gsub(params[:trigger_word], '').strip
+
+    action, repo = message.split('_').map {|c| c.strip.downcase }
+    repo_url = URI("https://api.github.com/repos/#{repo}")
+
+    case action
+      when 'issues'
+        resp = Net::HTTP.get(repo_url)
+        resp = JSON.parse(resp)
+        respond_message("There are #{resp['open_issues_count']} open issues on #{repo}")
+    end
   end
 
-  def respond_message message
-    content_type :json
-    {:text => message}.to_json
+  def respond_message(message)
+    {text: message}.to_json
   end
 end
