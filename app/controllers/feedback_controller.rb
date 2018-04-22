765fbc127afd7077200150f372592908d1b01c36
Iain Kinnish <iainkinnish@googlemail.com>
2018-04-22 19:25:06 +0100

    Testing the route

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -3,17 +3,7 @@ require 'net/http'
 class FeedbackController < ApplicationController
 
   def request_feedback
-    message = params[:text].gsub(params[:trigger_word], '').strip
-
-    action, repo = message.split('_').map {|c| c.strip.downcase }
-    repo_url = URI("https://api.github.com/repos/#{repo}")
-
-    case action
-      when 'issues'
-        resp = Net::HTTP.get(repo_url)
-        resp = JSON.parse resp.body
-        respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
-    end
+    render html: '<div>Testing</div>'.html_safe
   end
 
   def respond_message message
