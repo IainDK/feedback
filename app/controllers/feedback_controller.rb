a9a0f223042ecac774b8fb66974eb059caae2db6
Iain Kinnish <iainkinnish@googlemail.com>
2018-04-22 10:47:52 +0100

    Add API logic

---
+++ app/controllers/feedback_controller.rb
@@ -0,0 +1,23 @@
+require 'net/http'
+
+class FeedbackController < ApplicationController
+
+  def request_feedback
+    message = params[:text].gsub(params[:trigger_word], '').strip
+
+    action, repo = message.split('_').map {|c| c.strip.downcase }
+    repo_url = URI("https://api.github.com/repos/#{repo}")
+
+    case action
+      when 'issues'
+        resp = Net::HTTP.get(repo_url)
+        resp = JSON.parse resp.body
+        respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
+    end
+  end
+
+  def respond_message message
+    content_type :json
+    {:text => message}.to_json
+  end
+end
